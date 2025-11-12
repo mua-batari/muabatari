15daefad53b64c1f9fb053b900858c9ed30a5bfd
dils <adifuadil@gmail.com>
2025-11-12 14:41:16 +0700

    fix: Remove unused FaFacebook and FaTiktok imports from Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaInstagram, FaFacebook, FaTiktok } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   return (
